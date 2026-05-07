bd8459c07b56979520a86a125498141a536b454e
mcanadas29dev <mcanadas29dev@gmail.com>
2026-05-07 12:45:33 +0200

    "Html para readme"

--- regresion_lineal.py
+++ regresion_lineal.py
@@ -1,9 +1,10 @@
 import pandas as pd
-#import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
 
 # Vamos a ir paso a paso con el fichero Advertising.csv
 
@@ -12,11 +13,17 @@ from sklearn.linear_model import LinearRegression
 # Radio => Inversión en publicidad en radio (en miles de dólares).
 # Periodicos escritos => nversión en publicidad en prensa escrita (en miles de dólares).
 
+# =========================
+# 📂 CARGA DE DATOS
+# =========================
 # Sales = Ventas del producto(miles de unidades)
 df = pd.read_csv("ejemplos/Advertising.csv")
 print(df.head())
 print(df.info())
 
+# =========================
+# 📊 CORRELACIÓN
+# =========================
 # Calculamos la matriz de correlacion
 
 matriz = df.corr()
@@ -25,6 +32,11 @@ print(matriz)
 # Sales - Radio tienen un coef. de correlación de 0.57 positivo. 
 # La TV (input), targe (sales) vamos a intentar predecir que a mayor inversion en TV mayor sales
 # inversión en TV mas aumentan las Sales (vemtas)
+plt.figure(figsize=(8, 6))
+sns.heatmap(matriz, annot=True, cmap="coolwarm")
+plt.title("Matriz de correlación")
+plt.show()
+
 
 # Intentamos predecir 
 
@@ -34,8 +46,16 @@ X = df[['TV']]
 y = df['sales']
 
 # Entrenamiento del modelo (utilizamos regresion lineal por ser importes)
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
+
 # Dividimos el dataset en entrenamiento 80% y test 20%
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y_encoded, 
+    test_size=0.2, 
+    random_state=42)
+    #stratify=y_encoded)
+
 # Creamos el modelo de regresion lineal
 modelo = LinearRegression()
 # Entrenamos el modelo con los datos de entrenamiento
